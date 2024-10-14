982fb0c38137b9e45d1b5543065b7da56e0ad8e3
paolo flore <paoloflore937@gmail.com>
2024-10-14 22:18:45 +0200

    calcolo irpef a scaglioni in base al reddito

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,6 +39,29 @@ namespace SaaS.Controllers
             //Oltre €50.000: 43 %
             decimal Irpef = 0.0m;
 
+            if (lordo <= 15000)
+            {
+                Irpef = lordo * 0.23m;
+            }
+            else if (lordo > 15000 && lordo <= 28000)
+            {
+                Irpef = (15000 * 0.23m) + ((lordo - 15000) * 0.25m);
+            }
+            else if (lordo > 28000 && lordo <= 50000)
+            {
+                Irpef = (15000 * 0.23m) + (13000 * 0.25m) + ((lordo - 28000) * 0.35m);
+            }
+            else 
+            {
+                Irpef = (15000 * 0.23m) + (13000 * 0.25m) + (22000 * 0.35m) + ((lordo - 50000) * 0.43m);
+            }
+
+            // Addizionali Regionali e Comunali
+
+            //Addizionale regionale: varia generalmente dallo 0,9% al 3,33%.
+            //Addizionale comunale: generalmente varia dallo 0,1 % allo 0,9 %.
+
+
             decimal netto = lordo * 0.8m;
             decimal mesenetto = 0;
             decimal meselordo = 0;
